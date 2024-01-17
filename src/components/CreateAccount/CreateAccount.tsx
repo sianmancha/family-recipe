a2c0ca4ea1ab832103d3fe66d43afe5398a80fb7
sianmancha <manchasian@gmail.com>
2024-01-16 19:18:04 -0600

    Adds initial CreateAccount form with no styling

---
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {Formik, Form, Field} from 'formik'
+
+
+export function CreateAccount() {
+    return (
+        <div>
+            CreateAccount
+            <Formik
+                initialValues={{firstName: '', lastName: '', email: '', password: '', confirmPassword: ''}}
+                onSubmit={() => {}}
+            >
+                {({values, touched, handleChange, handleBlur, handleSubmit}) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Field 
+                            className='border-2'
+                            type="text"
+                            name="firstName"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.firstName}
+                            placeholder='First Name'
+                        />
+                        <Field 
+                            className='border-2'
+                            type="text"
+                            name="lastName"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.lastName}
+                            placeholder='Last Name'
+                        />
+                        <Field 
+                            className='border-2'
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            placeholder='Email'
+                        />
+                        <Field 
+                            className='border-2'
+                            type="text"
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            placeholder='Password'
+                        />
+                        <Field 
+                            className='border-2'
+                            type="text"
+                            name="confirmPSassword"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.confirmPassword}
+                            placeholder='Confirm Password'
+                        />
+                        <button type="submit">Create Account</button>
+                        <button>Cancel</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    )
+}
